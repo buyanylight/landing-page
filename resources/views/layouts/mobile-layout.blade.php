93314a984394c5e3d5b2dad9697a52e1a492c046
Rizvi <r.iqbal@almani.ae>
2019-07-08 18:05:20 +0400

    mobile popup

--- resources/views/layouts/mobile-layout.blade.php
+++ resources/views/layouts/mobile-layout.blade.php
@@ -39,7 +39,8 @@
     <link crossorigin="anonymous" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" rel="stylesheet">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="css/mobile.css">
+    <link rel="stylesheet" href="css/mobile.css?v=1">
+    <link rel="stylesheet" href="css/privacy.css?v=1">
 
 </head>
 <body>
