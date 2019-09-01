0755c0b29d84d28fcbff7fc29dcd194547486426
Tawing dela Rama <yabuking84@gmail.com>
2019-09-01 13:43:44 +0400

    changed white logo size

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -113,7 +113,7 @@
 		    flex-direction: column;">
 				<img class="bal-solo" 
 				style="width: 152px; margin-right: -10px;" 
-				src="{{ Util::assetUrl('images/logos/logo-white.png') }}">
+				src="{{ Util::assetUrl('images/logos/logo-white-sm.png') }}">
 				
 				<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
 			</div>
