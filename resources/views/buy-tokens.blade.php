4ec10791d6ff18c981b0404bdff5d49c1cd11545
Rizvi Iqbal <rzviqbal7@gmail.com>
2019-09-17 15:52:42 +0400

    Alert done

--- resources/views/buy-tokens.blade.php
+++ resources/views/buy-tokens.blade.php
@@ -44,7 +44,9 @@
 @section('content')
 
 <style type="text/css">
-	.toast
+	.toast{
+		display: none;
+	}
 
 </style>
 
@@ -71,17 +73,16 @@
 	  				<br>
 				</div>
 			</div>
-			<div class="toast toast-danger mt-2 w-100" role="alert" aria-live="polite" aria-atomic="true" data-autohide="false" style="max-width: 100% !important;">
+			<div class="toast toast-danger mt-2 w-100" role="alert" aria-live="polite" aria-atomic="true" data-autohide="false" style="max-width: 100% !important; border: 1px solid red">
 				<div class="toast-header">
-					<img src="..." class="rounded mr-2" alt="...">
-					<strong class="mr-auto">Bootstrap</strong>
-					<small class="text-muted">11 mins ago</small>
+					<img src="{{ Util::assetUrl('images/logo-black-icon.png') }}" width="25">
+					<strong class="mr-auto">&nbsp; BAL IEO</strong>
 					<button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
 		      			<span aria-hidden="true">&times;</span>
 		    		</button>
 		  		</div>
 		  		<div class="toast-body">
-		    		Hello, world! This is a toast message.
+		    		The minimum investment is 5000 USD.
 		  		</div>
 			</div>
 			<div class="pt-4">
