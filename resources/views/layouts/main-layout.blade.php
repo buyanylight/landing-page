c95fcd20b6232b5ca3758d5ad12d9cd32db65eaa
Rizvi Iqbal <rzviqbal7@gmail.com>
2019-09-28 09:24:19 +0400

    testing on safari

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -161,6 +161,7 @@
 		<!-- Scripts -->
 		<script src="{{ Util::assetUrl('js/app.js') }}" charset="utf-8"></script>
 		<script src="{{ Util::assetUrl('js/validator.min.js') }}" charset="utf-8"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
 		<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 	 	<script src="https://www.google.com/recaptcha/api.js?render={{ env('CAPTCHA_KEY') }}"></script>
