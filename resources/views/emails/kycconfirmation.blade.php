d71f276233760b74933f29ce00a264b67e63e4eb
Rizvi Iqbal <rzviqbal7@gmail.com>
2019-10-05 14:44:53 +0400

    changed email contents

--- resources/views/emails/kycconfirmation.blade.php
+++ resources/views/emails/kycconfirmation.blade.php
@@ -8,18 +8,18 @@
 	</p>
 	<ul style=" color: black; list-style: none;">
 		<li><b>Reference ID:</b> {{ $user_reference_id }}</li>
-		<li style="padding-top: 10px;"><b>Name:</b> {{ $name }}</li>
-		<li style="padding-top: 10px;"><b>Email:</b> {{ $email }}</li>
-		<li style="padding-top: 10px;"><b>Mobile:</b> {{ $number }}</li>
-		<li style="padding-top: 10px;"><b>BAL Tokens Total Value:</b> {{ $bal_amt }}</li>
-		<li style="padding-top: 10px;"><b>Amount Paid:</b> {{ $amount }}</li>
-		<li style="padding-top: 10px;"><b>Country:</b> {{ $country }}</li>
-		<li style="padding-top: 10px;"><b>Your ETH address:</b> {{ $receiver_id }}</li>
-		<li style="padding-top: 10px;"><b>User Verification ID:</b> Received</li>
-		<li style="padding-top: 10px;"><b>User Selfie Verification:</b> Received</li>
+		<li style="padding-top: 5px;"><b>Name:</b> {{ $name }}</li>
+		<li style="padding-top: 5px;"><b>Email:</b> {{ $email }}</li>
+		<li style="padding-top: 5px;"><b>Mobile:</b> {{ $number }}</li>
+		<li style="padding-top: 5px;"><b>BAL Tokens Total Value:</b> {{ $bal_amt }}</li>
+		<li style="padding-top: 5px;"><b>Amount Paid:</b> {{ $amount }}</li>
+		<li style="padding-top: 5px;"><b>Country:</b> {{ $country }}</li>
+		<li style="padding-top: 5px;"><b>Your ETH address:</b> {{ $receiver_id }}</li>
+		<li style="padding-top: 5px;"><b>User Verification ID:</b> Received</li>
+		<li style="padding-top: 5px;"><b>User Selfie Verification:</b> Received</li>
 	</ul>
 	<p style="color: black; font-size: 14px">
-		You will receive your {{ $bal_amt }} BAL Tokens very soon. We will send them to the following ETH address: {{ $receiver_id }}
+		We will secure the transfer of {{ $bal_amt }} BAL tokens into your account as immediately as possible. After this part: You may check the amount in your ETH address: [{{ $receiver_id }}] thereafter. You may also check <a href=" https://kb.myetherwallet.com/en/tokens/how-to-add-custom-token/"> this link</a> on how you can add custom tokens to your ETH wallet hassle-free.
 	</p>
 	<p style="color: black; font-size: 14px">
 		It is a pleasure to serve you. Connect with all of our channels if you need further assistance and cooperation.
