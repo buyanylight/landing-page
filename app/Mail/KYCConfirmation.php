f103875bd16b67dd8798d0d84a65f1b9cc0179b3
Rizvi Iqbal <rzviqbal7@gmail.com>
2019-11-24 14:38:49 +0400

    u

--- app/Mail/KYCConfirmation.php
+++ app/Mail/KYCConfirmation.php
@@ -35,16 +35,6 @@ class KYCConfirmation extends Mailable
     {
 
 
-        //  function generateRandomString($length) {
-        //     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        //     $charactersLength = strlen($characters);
-        //     $randomString = '';
-        //     for ($i = 0; $i < $length; $i++) {
-        //         $randomString .= $characters[rand(0, $charactersLength - 1)];
-        //     }
-        //     return $randomString;
-        // }
-
 
         $curl = curl_init();
 
